b5ab89733c0ede49507433fb808af27afe4c302a
TheShovel <niccata24@gmail.com>
2023-12-29 21:10:33 +0200

    Update main.go

--- main.go
+++ main.go
@@ -112,10 +112,8 @@ func jsonSuccess(w http.ResponseWriter) {
 }
 
 func main() {
-	log.Println("Path is "+os.Getenv("RAILWAY_VOLUME_MOUNT_PATH"))
-	log.Println("DB file at "+os.Getenv("RAILWAY_VOLUME_MOUNT_PATH")+"/db.sqlite")
 	// Initialise database connection
-	db, err := sql.Open("sqlite", os.Getenv("RAILWAY_VOLUME_MOUNT_PATH")+"/db.sqlite")
+	db, err := sql.Open("sqlite", "/data/db.sqlite")
 	if err != nil {
 		log.Fatalln(err)
 	}
